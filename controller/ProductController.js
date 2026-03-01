b1e71ff7b58728d098209945567a94159f0722e8
jayrajrao <jayrajrao15@gmail.com>
2026-03-01 16:05:53 +0530

    Update product controller

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -34,7 +34,7 @@ static create = async (req, res) => {
 
     const uploadImage = await cloudinary.uploader.upload(
       req.files.images.tempFilePath,
-      { folder: "productimage" }
+      { folder: "products" }
     );
 
     const product = await ProductModel.create({
